e9f4238c1c8afcd5f5baa7a92f7aafc2959cd68c
Christerresin <christer.resin@gmail.com>
2022-11-09 14:23:46 +0100

    create class for Car entity

---
+++ src/main/java/com/example/parkingspot/car/Car.java
@@ -0,0 +1,45 @@
+package com.example.parkingspot.car;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String registrationNr;
+  private Long ownerId;
+
+  public Car() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getRegistrationNr() {
+    return registrationNr;
+  }
+
+  public void setRegistrationNr(String registrationNr) {
+    this.registrationNr = registrationNr;
+  }
+
+  public Long getOwnerId() {
+    return ownerId;
+  }
+
+  public void setOwnerId(Long ownerId) {
+    this.ownerId = ownerId;
+  }
+
+}
