4c37b562b5cbf690c13091a01a5fa2ae337c193c
Christerresin <christer.resin@gmail.com>
2022-11-10 14:56:35 +0100

    add ZoneService and ZoneRepository

---
+++ src/main/java/com/example/parkingspot/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.example.parkingspot.zone;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository {
+
+}
---
+++ src/main/java/com/example/parkingspot/zone/ZoneService.java
@@ -0,0 +1,16 @@
+package com.example.parkingspot.zone;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ZoneService {
+  private ZoneRepository zoneRepository;
+
+  public ZoneService(ZoneRepository zoneRepository) {
+    this.zoneRepository = zoneRepository;
+  }
+
+  public void registerNewParkingZone(Zone zone) {
+
+  }
+}
